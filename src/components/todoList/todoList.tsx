8ebb7dee89aaa3ef26706edfeda94852413d9f47
Erez Birenholz <biran330@gmail.com>
2022-01-20 15:42:37 +0200

    Update todoList.tsx

--- src/components/todoList/todoList.tsx
+++ src/components/todoList/todoList.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import classes from "./todoList.module.css";
-import { TotoItem } from "../../models";
+import { TodoItemModel } from "../../models";
+import TodoItem from "./todoItem/todoItem";
 
-const todos: TotoItem[] = [
+const todos: TodoItemModel[] = [
   { id: "1", title: "Feed dog" },
   { id: "2", title: "Go for walk" },
+  { id: "3", title: "Write essay", description: "- electric -water" },
 ];
+
+const handleDeleteItem = () => {
+  console.log("deleted");
+};
 
 const TodoList = () => {
   return (
     <div>
-      <ul>
+      <ul className={classes.ul}>
         {todos.map((todoItem) => (
-          <li className={classes.list} key={todoItem.id}>
-            <input type="checkbox"></input>
-            <label for={todoItem.title} contentEditable>
-              {todoItem.title}
-            </label>
-          </li>
+          <TodoItem
+            key={todoItem.id}
+            todoItem={todoItem}
+            handleDeleteItem={handleDeleteItem}
+          ></TodoItem>
         ))}
       </ul>
     </div>
