2de4becddba415e0fdea74f7c440d38e87f9e46d
Erez Birenholz <biran330@gmail.com>
2022-01-26 13:02:52 +0200

    Update todoList.tsx

--- src/components/todoList/todoList.tsx
+++ src/components/todoList/todoList.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import classes from "./todoList.module.css";
 import TodoItem from "./todoItem/todoItem";
 import AddTaskButton from "./addTaskButton/addTaskButton";
 import { useTodosStore } from "../../models/todosStore";
 import { observer } from "mobx-react";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoList = () => {
+  const listRef = useRef();
+
   const todoStore = useTodosStore();
 
   const handleDeleteItem = (todoID: string) => {
@@ -16,15 +19,22 @@ const TodoList = () => {
     todoStore.setSelectedTodo(todoID);
   };
 
+  const handleInsert = async (index: number) => {
+    const newTodo = { id: uuidv4(), title: "" };
+    await todoStore.insertTodo(newTodo, index);
+  };
+
   return (
     <div>
-      <ul className={classes.ul}>
+      <ul className={classes.ul} ref={listRef}>
         {todoStore.todos.map((todoItem) => (
           <li key={todoItem.id}>
             <TodoItem
+              listRef={listRef}
               todoItem={todoItem}
               handleSelected={handleSelected}
               handleDelete={handleDeleteItem}
+              handleInsert={handleInsert}
             ></TodoItem>
           </li>
         ))}
