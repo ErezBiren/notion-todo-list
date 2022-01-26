65c1c2af2784706880c4679cf4bd728593cf50a3
Erez Birenholz <biran330@gmail.com>
2022-01-26 14:52:49 +0200

    Update addTaskButton.tsx

--- src/components/todoList/addTaskButton/addTaskButton.tsx
+++ src/components/todoList/addTaskButton/addTaskButton.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { useTodosStore } from "../../../models/todosStore";
 import classes from "./addTaskButton.module.css";
-import { v4 as uuidv4 } from "uuid";
 
-const AddTaskButton = () => {
-  const todosStore = useTodosStore();
+interface AddTaskButtonProps {
+  handleAddTodo: () => void;
+}
 
-  const handleAddTask = () => {
-    const newTodo = { id: uuidv4(), title: "" };
-    todosStore.addTodo(newTodo);
-  };
-
+const AddTaskButton = ({ handleAddTodo }: AddTaskButtonProps) => {
   return (
-    <div className={classes.addTask} onClick={handleAddTask}>
+    <div className={classes.addTask} onClick={handleAddTodo}>
       + Add Task
     </div>
   );
